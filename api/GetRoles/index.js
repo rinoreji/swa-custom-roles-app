c80441695de44a4ab2826e50a449318c54813880
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-09-19 12:50:41 +0530

    trying admin role

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch').default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
-    'admin': '21a96550-aa02-486e-9297-e6e51b6398fc',
+    'admin': '43ab041e-c0dc-40a1-a740-d58c964de408',
     'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
 };
 
